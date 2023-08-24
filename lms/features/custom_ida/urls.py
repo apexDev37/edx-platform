d3b00d416caf5d2949bc4ca52daee5eb4aed5767
apexdev <mwangi.em37@gmail.com>
2023-08-24 11:04:49 +0300

    fix: Update path config
    error:
    Resolve 404 error when trying to access custom LMS endpoint.
    expected:
    JSON response with a custom welcome message.
    actual:
    Django's - Page Not Found (404) error.
    fix:
    - Modify the registered and expected LMS path: "customida/ customida/".
    - Remove duplicate path resource "customida/" in app path
    replace with "api/v1/welcome/" .

--- lms/features/custom_ida/urls.py
+++ lms/features/custom_ida/urls.py
@@ -4,5 +4,5 @@ from .views import CustomIDAResponseView
 app_name = 'custom_ida'
 
 urlpatterns = [
-  path('customida/', CustomIDAResponseView.as_view(), name='custom_ida')
+  path('api/v1/welcome/', CustomIDAResponseView.as_view(), name='custom_ida')
 ]
