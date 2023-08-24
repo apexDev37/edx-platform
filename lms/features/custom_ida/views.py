4c3bc7ad1f1579d6ba875cc3409735c8d8a15e88
apexdev <mwangi.em37@gmail.com>
2023-08-24 10:45:14 +0300

    fix: Utilize rest_framework
    error:
    "ModuleNotFoundError: No module named 'openedx.core.djangoapps.content.views'"
    cause:
    "from openedx.core.djangoapps.content.views.base import BaseView"
    fix:
    Replace incorrect import and prefer rest_framework for API definition.
    additional:
    Prefer rest_framework 'Response' over standard Django JSON response.

--- lms/features/custom_ida/views.py
+++ lms/features/custom_ida/views.py
@@ -1,7 +1,7 @@
-from django.http import JsonResponse
-from openedx.core.djangoapps.content.views.base import BaseView
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-class CustomIDAResponseView(BaseView):
+class CustomIDAResponseView(APIView):
   def greet(self, request, *args, **kwargs):
     payload = {'message': 'Welcome to Open edX!'}
-    return JsonResponse(payload)
+    return Response(payload)
