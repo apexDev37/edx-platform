ec15b40d99e44deea905fbd54057dff8f3529f81
apexdev <mwangi.em37@gmail.com>
2023-08-24 11:14:08 +0300

    fix: Change view name
    error:
    HTTP 405 Method Not Allowed
    expected:
    A custom welcome JSON response from hitting the endpoint.
    actual:
    DRF error response page with a 405 Method Not Allowed.
    fix:
    Follow DRF rule for view method name to map to intended HTTP method.
    Modify view name from "greet" to override desired HTTP method "GET".

--- lms/features/custom_ida/views.py
+++ lms/features/custom_ida/views.py
@@ -2,6 +2,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 class CustomIDAResponseView(APIView):
-  def greet(self, request, *args, **kwargs):
+  def get(self, request, *args, **kwargs):
     payload = {'message': 'Welcome to Open edX!'}
     return Response(payload)
